59d376df5c6ffb61db5879b3d50ebdbb51da84c9
Brooke Heck <b19heck@gmail.com>
2022-08-16 10:03:39 -0700

    add styling around best section

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -6,7 +6,7 @@ class HornedBeast extends React.Component {
   render() {
     return (
       <>
-        <section>
+        <section className="hornedBeast">
           <h2>{this.props.title}</h2>
           <img src={this.props.imageUrl} alt={this.props.imageAlt} title={this.props.imageAlt}></img>
           <p>{this.props.description}</p>
