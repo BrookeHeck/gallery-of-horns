5e3dfa9efe92f46f14ee03aa06be89e0c1c04a4a
Brooke Heck <b19heck@gmail.com>
2022-08-16 12:08:03 -0700

    add boostrap cards

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,25 +1,26 @@
 import React from "react";
 import './HornedBeast.css';
+import Card from 'react-bootstrap/Card';
 
 class HornedBeast extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      votes: 0
+      likes: 0
     }
   }
 
-  handleVote = () => this.setState({votes: this.state.votes + 1});
+  handleLike = () => this.setState({likes: this.state.likes + 1});
 
   render() {
     return (
       <>
-        <section className="hornedBeast">
-          <h2>{this.props.title} <sup>{this.state.votes}</sup></h2>
-          <p className="vote" onClick={this.handleVote}>Vote</p>          
-          <img src={this.props.imageUrl} alt={this.props.imageAlt} title={this.props.imageAlt}></img>
-          <p>{this.props.description}</p>
-        </section>
+        <Card className="beastCard">
+          <Card.Img variant="top" src={this.props.imageUrl} alt={this.props.imageAlt} title={this.props.imageAlt} />
+          <Card.Title>{this.props.title} <sup>{this.state.likes}</sup></Card.Title>
+          <Card.Text>{this.props.description}</Card.Text>
+          <div className="likeDiv" onClick={this.handleLike}>Like 💖</div>
+        </Card>
       </>
     )
   };
--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './App.css';
+import './Main.css';
 import HornedBeast from "./HornedBeast";
 import data from "./data.json"
 
