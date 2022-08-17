971d192760886504508d9b78f4d7ea6fb3a337ac
Brooke Heck <b19heck@gmail.com>
2022-08-17 12:57:57 -0700

    add a model on img click

---
+++ src/SelectedBeast.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './SelectedBeast.css';
+import Modal from 'react-bootstrap/Modal';
+
+class SelectedBeast extends React.Component {
+
+  render() {
+    return (
+      <>
+        <Modal show={this.props.showBeast}>
+          <Modal.Header closeButton onClick={this.props.handleModalClose}>
+            <Modal.Title>{this.props.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <img src={this.props.url} alt={this.props.alt} title={this.props.title}/>
+          </Modal.Body>
+        </Modal>
+      </>
+    )
+  };
+
+} 
+
+export default SelectedBeast;
