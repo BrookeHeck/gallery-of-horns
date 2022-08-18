7e59335527c9f60a8d4cd5f68286887e9aa428bf
Brooke Heck <b19heck@gmail.com>
2022-08-18 11:50:39 -0700

    fix the function firing before the state finishes setting

--- src/FilterForm.js
+++ src/FilterForm.js
@@ -12,28 +12,24 @@ class FilterForm extends React.Component {
   }
 
   handleEnteredSearch = (event) => {
-    this.setState({searchStr: event.target.value});
-    this.props.handleSearch(this.state.searchStr);
+    this.setState({searchStr: event.target.value}, () => {this.props.handleSearch(this.state.searchStr)});
   }
 
   handleHornFilter = (event) => {
-    console.log(event.target.value);
-    this.setState({hornsSelected: Number.parseInt(event.target.value)});
-    console.log(this.state.hornsSelected);
-    this.props.handleHornSearch(this.state.hornsSelected);
+    this.setState({hornsSelected: Number.parseInt(event.target.value)}, () => {this.props.handleHornSearch(this.state.hornsSelected)});
   }
 
   render() {
     return (
       <Form className="searchForm">
         <Form.Group className="mb-3">
-          <Form.Control type="text" placeholder="Search" onChange={this.handleEnteredSearch.bind(this)}/>
-          <Form.Select aria-label="Default select example" onChange={this.handleHornFilter.bind(this)}>
+          <Form.Control type="text" placeholder="Search" onChange={this.handleEnteredSearch}/>
+          <Form.Select aria-label="Default select example" onChange={this.handleHornFilter}>
             <option value="0">Search by Number of Horns</option>
             <option value="1">One</option>
             <option value="2">Two</option>
             <option value="3">Three</option>
-    </Form.Select>
+          </Form.Select>
         </Form.Group>
       </Form>
     )
